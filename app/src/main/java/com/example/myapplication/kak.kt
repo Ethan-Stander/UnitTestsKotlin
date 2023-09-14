61dfd75e2fe75d78eb78633dcded2b0b8b10fceb
OofSpooder <spoodermankilledamerica@gmail.com>
2023-09-14 09:06:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/kak.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+class kak {
+
+    companion object{
+
+    fun English() : String
+    {
+        return "Kak";
+    }
+
+
+    fun Afrikaans() : String
+    {
+        return "Kak";
+    }
+
+    fun Xhosa() : String
+    {
+        return "Kak"
+    }
+    }
+}
